c28015fa257830819860f9510c497cd660d6ef62
LuisRcap <luisdany7773@gmail.com>
2021-11-26 22:01:49 -0600

    Tipos basicos :brain:

--- 01-reforzamiento/src/typescript/TiposBasicos.tsx
+++ 01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,8 +1,19 @@
 
 const TiposBasicos = () => {
+
+    const nombre: string = 'Luis';
+    const edad: number = 26;
+    const estaActivo: boolean = true;
+
+    const poderes: string[] = [];   // 'Velocidad', 'Volar', 'Respirar bajo el agua'
+    // const poderes: (string | number)[]
+
     return (
         <>
             <h3>Tipos Básicos</h3>
+            { nombre }, { edad }, { (estaActivo) ? 'Activo' : 'Inactivo' }
+            <br />
+            { poderes.join(', ') }
         </>
     )
 }
