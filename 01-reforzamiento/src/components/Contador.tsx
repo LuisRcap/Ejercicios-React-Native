ffb14d5951a227e60e0c0e9c145c0075b09f5160
LuisRcap <luisdany7773@gmail.com>
2021-11-27 15:23:38 -0600

    useState :fire:

---
+++ 01-reforzamiento/src/components/Contador.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const Contador = () => {
+
+    const [valor, setValor] = useState( 0 );
+
+    const acumular = ( numero: number ) => {
+        setValor( valor + numero )
+    };
+
+    return (
+        <>
+            <h3>Contador <small>{ valor }</small></h3>
+            <button
+                className="btn btn-primary me-3"
+                onClick={ () => acumular( 1 ) }
+            >
+                + 1
+            </button>
+
+            <button
+                className="btn btn-primary"
+                onClick={ () => acumular( -1 ) }
+            >
+                - 1
+            </button>
+        </>
+    )
+}
+
+export default Contador;
