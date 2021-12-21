b1c0dee0217565bfd21d47180da8cdc6b2b3604e
LuisRcap <luisdany7773@gmail.com>
2021-12-20 22:08:43 -0600

    TouchableOpacity :brain:

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { useState } from 'react';
 
 const ContadorScreen = () => {
@@ -21,10 +21,32 @@ const ContadorScreen = () => {
                 Contador: { contador }
             </Text>
 
-            <Button 
-                title='Click'
-                onPress={ () => setContador( contador + 1 ) }
-            />
+            <TouchableOpacity
+                onPress={ () => setContador( contador +1 ) }
+                style={{
+                    width: 50,
+                    alignSelf: 'center'
+                }}
+            >
+                <View style={{
+                    backgroundColor: '#2196F3',
+                    borderRadius: 100,
+                    width: 50,
+                    height: 50,
+                    alignSelf: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <Text style={{
+                        textAlign: 'center',
+                        color: "white",
+                        fontSize: 22,
+                        fontWeight: 'bold',
+                        top: -2
+                    }}>
+                        +1
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
