5f41793051d217ef63788dd3d70d5c893239ec39
LuisRcap <luisdany7773@gmail.com>
2021-12-20 21:53:28 -0600

    Crear contador :sparkles:

---
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button, Text, View } from 'react-native';
+import { useState } from 'react';
+
+const ContadorScreen = () => {
+
+    const [contador, setContador] = useState(10);
+
+
+
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center'
+        }}>
+            <Text style={{
+                textAlign: 'center',
+                fontSize: 40,
+                top: -15
+            }}>
+                Contador: { contador }
+            </Text>
+
+            <Button 
+                title='Click'
+                onPress={ () => setContador( contador + 1 ) }
+            />
+        </View>
+    )
+}
+
+export default ContadorScreen;
