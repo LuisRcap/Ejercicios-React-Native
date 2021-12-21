38ba74f42bc2affde28659568473748c17ccc9e3
LuisRcap <luisdany7773@gmail.com>
2021-12-20 21:30:58 -0600

    Pantallas independientes :brain:

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import HolaMundoScreen from './src/screens/HolaMundoScreen';
 
 
 const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center'
-    }}>
-      <Text style={{
-        fontSize: 45,
-        textAlign: 'center'
-      }}>
-        Hola Mundo
-      </Text>
-    </View>
+    <HolaMundoScreen />
   )
 }
 
---
+++ 02-MiPrimeraApp/src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HolaMundoScreen = () => {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center'
+          }}>
+            <Text style={{
+              fontSize: 45,
+              textAlign: 'center'
+            }}>
+              Hola Luis
+            </Text>
+            
+        </View>
+    )
+}
+
+export default HolaMundoScreen
