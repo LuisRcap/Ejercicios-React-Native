746f26b852c5d50b143ae8fbf2725fb1dad06732
LuisRcap <luisdany7773@gmail.com>
2021-12-21 21:17:51 -0600

    StyleSheet

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 import { useState } from 'react';
 
 const ContadorScreen = () => {
@@ -9,40 +9,17 @@ const ContadorScreen = () => {
 
 
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center'
-        }}>
-            <Text style={{
-                textAlign: 'center',
-                fontSize: 40,
-                top: -15
-            }}>
+        <View style={ styles.container }>
+            <Text style={ styles.title }>
                 Contador: { contador }
             </Text>
 
             <TouchableOpacity
                 onPress={ () => setContador( contador +1 ) }
-                style={{
-                    width: 50,
-                    alignSelf: 'center'
-                }}
+                style={ styles.boton_wrap }
             >
-                <View style={{
-                    backgroundColor: '#2196F3',
-                    borderRadius: 100,
-                    width: 50,
-                    height: 50,
-                    alignSelf: 'center',
-                    justifyContent: 'center'
-                }}>
-                    <Text style={{
-                        textAlign: 'center',
-                        color: "white",
-                        fontSize: 22,
-                        fontWeight: 'bold',
-                        top: -2
-                    }}>
+                <View style={ styles.boton }>
+                    <Text style={ styles.text_boton }>
                         +1
                     </Text>
                 </View>
@@ -50,5 +27,36 @@ const ContadorScreen = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: -15
+    },
+    boton_wrap: {
+        width: 50,
+        alignSelf: 'center'
+    },
+    boton: {
+        backgroundColor: '#2196F3',
+        borderRadius: 100,
+        width: 50,
+        height: 50,
+        alignSelf: 'center',
+        justifyContent: 'center'
+    },
+    text_boton: {
+        textAlign: 'center',
+        color: "white",
+        fontSize: 22,
+        fontWeight: 'bold',
+        top: -2
+    }
+});
 
 export default ContadorScreen;
