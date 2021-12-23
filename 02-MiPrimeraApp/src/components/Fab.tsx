4e740307268482fbef3776e44e2bd7d2d49fd0fd
LuisRcap <luisdany7773@gmail.com>
2021-12-22 19:42:40 -0600

    Estilo condicional :art:

--- 02-MiPrimeraApp/src/components/Fab.tsx
+++ 02-MiPrimeraApp/src/components/Fab.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
 
 interface Props {
     title: string;
+    position?: 'br' | 'bl';
     onPress: () => void;
 }
 
-const Fab = ( { title, onPress }: Props) => {
-
-    console.log(title);
+const Fab = ( { title, position = 'br', onPress }: Props) => {
 
     return (
-        <TouchableOpacity
+
+        <View style={[ styles.fabLocation, ( position === 'bl' ) ? styles.left : styles.right ]} >
+
+        <TouchableNativeFeedback 
             onPress={ onPress }
-            style={ styles.boton_wrapBR }
+            background={ TouchableNativeFeedback.Ripple('#28425B', false, 30) }
         >
             <View style={ styles.fab }>
                 <Text style={ styles.text_boton }>
                     { title }
                 </Text>
             </View>
-        </TouchableOpacity>
+        </TouchableNativeFeedback>
+        </View>
+
     )
 }
 
 const styles = StyleSheet.create({
-    boton_wrapBR: {
+
+    fabLocation: {
         bottom: 25,
-        position: 'absolute',
-        right: 25,
-        width: 50
+        position: 'absolute'
+    },
+    right: {
+        right: 25
+    },
+    left: {
+        left: 25
     },
     fab: {
         alignSelf: 'center',
@@ -37,7 +46,16 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         height: 60,
         justifyContent: 'center',
-        width: 60
+        width: 60,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+
+        elevation: 7,
     },
     text_boton: {
         alignSelf: 'center',
